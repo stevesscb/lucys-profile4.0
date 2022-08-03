b403e185b56204e935cadf04ac39d7d5c5c2a316
stevesscb <stevesscb@gmail.com>
2022-08-03 08:31:54 +0800

    structure page layout

---
+++ src/pages/features.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+
+const FeaturesPage = () => {
+  return (
+    <Layout pageTitle='Features Page'>
+      <p>This is the features page!</p>
+    </Layout>
+  );
+};
+
+export default FeaturesPage;
---
+++ src/pages/reviews.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+
+const ReviewsPage = () => {
+  return (
+    <Layout pageTitle='Reviews Page'>
+      <p>This is the Reviews page!</p>
+    </Layout>
+  );
+};
+
+export default ReviewsPage;
